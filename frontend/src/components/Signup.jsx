e4a7dde352c57bcd65aa12611ef11db9c7ce14b0
julia.capitanescu@student.upt.ro <m53CrxK#>
2026-03-24 00:04:49 +0200

    Authentication system

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,25 +1,51 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext"
 
 const Signup = () => {
 const [username, setUsername] = useState("")
 const [email, setEmail] = useState("")
 const [password, setPassword] = useState("")
+const [confirmPassword, setConfirmPassword] = useState("")
 const [error, setError] = useState("")
 const [loading, setLoading] = useState("")
 
-const {session} = UserAuth();
+const {session, signUpNewUser} = UserAuth();
+const navigate = useNavigate();
 console.log(session);
+//console.log(username, email, password, confirmPassword);
+
+const handleSignUp = async (e) => {
+	e.preventDefault();
+
+	if(password !== confirmPassword) {
+		setError("Passwords do not match");
+		return;
+	}
+
+	setLoading(true);
+
+	try {
+		const result = await signUpNewUser({ username, email, password });
+		if (result.success) {
+			navigate('/dashboard');
+		}
+	} catch (error) {
+		setError("Error occured");
+	} finally {
+		setLoading(false);
+	}
+};
 
 	return (
 		<div>
-			<form className="max-w-lg m-auto pt-24">
+			<form onSubmit={handleSignUp} className="max-w-lg m-auto pt-5">
 				<h2 className="font-bold text-center pb-6">Sign Up</h2>
 				<div className="flex flex-col justify-center items-center  gap-4 mb-4">
 					<label className="floating-label w-[60%]">
 						<span>Username</span>
 						<input
+							onChange={(e) => setUsername(e.target.value)}
 							type="text"
 							placeholder="Username"
 							className="input input-md w-full"
@@ -28,6 +54,7 @@ console.log(session);
 					<label className="floating-label w-[60%]">
 						<span>Email</span>
 						<input
+							onChange={(e) => setEmail(e.target.value)}
 							type="email"
 							placeholder="Email"
 							className="input input-md w-full "
@@ -36,6 +63,7 @@ console.log(session);
 					<label className="floating-label w-[60%]">
 						<span>Password</span>
 						<input
+							onChange={(e) => setPassword(e.target.value)}
 							type="password"
 							placeholder="Password"
 							className="input input-md w-full"
@@ -44,12 +72,15 @@ console.log(session);
 					<label className="floating-label w-[60%]">
 						<span>Confirm password</span>
 						<input
+							onChange={(e) => setConfirmPassword(e.target.value)}
 							type="password"
 							placeholder="Confirm password"
 							className="input input-md w-full"
 						/>
 					</label>
-					<button type="submit" disabled={ loading } className="btn rounded-lg mt-4 btn-outline btn-primary w-[50%] ">Create account</button>
+					{error && <p className="text-xs text-error text-center">{error}</p>}
+					<button type="submit" disabled={ loading } className="btn rounded-lg btn-outline btn-primary w-[50%] ">Create account</button>
+					
 				</div>
 				<p className="text-sm text-gray-600">
 					Already have an account?{" "}
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState, useContext } from "react";
+import { createContext, useEffect, useState, useContext, use } from "react";
 import { supabase } from "../supabaseClient";
 
 const AuthContext = createContext()
@@ -6,8 +6,57 @@ const AuthContext = createContext()
 export const AuthContextProvider = ({children}) => {
     const [session, setSession] = useState(undefined)
 
+    // Sign up
+    const signUpNewUser = async ({ username, email, password }) => {
+        const { data, error } = await supabase.auth.signUp({  email: email, password: password, options: { data: { username: username } } });
+
+        if (error){
+            console.error("Error signing up:", error);
+            return { success: false, error };
+        }
+        return { success: true, data };
+    };
+
+    // Sign in
+    const signInUser = async ({ email, password }) => {
+        try {
+            const { data, error } = await supabase.auth.signInWithPassword({ email: email, password: password });
+
+            if (error) {
+                console.error("Error signing in:", error);
+                return { success: false, error: error.message };
+            }
+            
+            console.log("Sign-in successful:", data);
+            return { success: true, data };
+
+        } catch (error) {
+            console.error("Error occured:", error);
+        }
+    };
+
+    useEffect(() => {
+
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+
+    }, []);
+
+    //Sign out
+    const signOut = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.error("Error signing out:", error);
+        }
+    };
+
     return (
-        <AuthContext.Provider value={{session}}>
+        <AuthContext.Provider value={{session, signUpNewUser, signInUser, signOut }}>
             {children}
         </AuthContext.Provider>
     )
